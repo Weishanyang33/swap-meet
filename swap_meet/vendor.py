714afc5f2469b4b284037f3dc5692443b1ed5368
weishanyang <weishanyang33@gmail.com>
2021-04-01 14:16:11 -0700

    create add method in Vendor class.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -6,4 +6,7 @@ class Vendor:
             self.inventory = []
         else:
             self.inventory = inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
     
