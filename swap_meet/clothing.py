a8f40f911500a7a07c5fcd589e4166843a52e59a
weishanyang <weishanyang33@gmail.com>
2021-04-01 20:29:07 -0700

    add condition attribute in Clothing constructor.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self):
-        super().__init__(category = "Clothing")
+    def __init__(self, condition = 0):
+        super().__init__(condition = condition, category = "Clothing")
     def __str__(self):
         return "The finest clothing you could wear."
