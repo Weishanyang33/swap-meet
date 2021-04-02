58a5ea9c8a8030a923236b2b6a2b53011675fceb
weishanyang <weishanyang33@gmail.com>
2021-04-01 20:30:39 -0700

    add condition attribute in Decor constructor.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self):
-        super().__init__(category = "Decor")
+    def __init__(self, condition):
+        super().__init__(condition = condition, category = "Decor")
     def __str__(self):
         return "Something to decorate your space."
