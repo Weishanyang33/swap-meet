5d55a57ef37208da46a29b13cae7aacdc74aaa41
weishanyang <weishanyang33@gmail.com>
2021-04-01 20:31:46 -0700

    add condition attribute in electronics constructor.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self):
-        super().__init__(category = "Electronics")
+    def __init__(self, condition = 0):
+        super().__init__(condition = condition, category = "Electronics")
     def __str__(self):
         return "A gadget full of buttons and secrets."
