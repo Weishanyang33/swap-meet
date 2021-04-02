6d27dd9cf446768cba28c2ad286527b070a38dbb
weishanyang <weishanyang33@gmail.com>
2021-04-01 21:11:32 -0700

    create condition_description method in Item class.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,4 +4,18 @@ class Item:
         self.condition = condition
     
     def __str__(self):
-        return "Hello World!"        
+        return "Hello World!"   
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Take it home for free!"
+        elif self.condition == 1:
+            return "Bearable"
+        elif self.condition == 2:
+            return "Usable"
+        elif self.condition == 3:
+            return "Normal wear"
+        elif self.condition == 4:
+            return "Like new"
+        elif self.condition == 5:
+            return "Brand-New"
